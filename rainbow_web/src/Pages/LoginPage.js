6368f768ed72ca642c5dc0b6b5445396c9dc9f73
Yoomin Hwang <yoominh3@gmail.com>
2024-07-03 15:21:25 +0900

    Fix: LoginPage 내의 필요 없는 주석 삭제 및 안 쓰는 GoogleLoginBtn component 삭제

--- rainbow_web/src/Pages/LoginPage.js
+++ rainbow_web/src/Pages/LoginPage.js
@@ -8,19 +8,6 @@ import LoginHeader from '../Components/LoginHeader';
 const google = window.google;
 const clientId = process.env.REACT_APP_GOOGLE_AUTH_CLIENT_ID;
 
-// {/* Sign Out button 이 로그인 성공 시에만 보여지도록 하는 코드 */}
-// { Object.keys(user).length != 0 &&
-//   <button onClick={ (e) => handleSignOut(e)}>Sign Out</button>
-// }
-
-// {/* 로그인 성공 시, 즉 user 변수에 어떤 값이 들어갈 시, 유저 프로필과 이름이 보여지도록 하는 코드 */}
-// { user &&
-//   <div>
-//     <img src={user.picture}></img>
-//     <h3>{user.name}</h3>
-//   </div>
-// }
-
 function LoginPage() {
   const [ user, setUser ] = useState({});
 
