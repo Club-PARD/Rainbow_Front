d14d888e4b62ec13fd44abad06c30841e649ebeb
codms0201 <chanyeol0615@naver.com>
2024-07-03 20:18:50 +0900

    Fix: book, list, 구글버튼 컴포넌트 삭제 및 커뮤니티 페이지 생성, 헤더.js 수정

---
+++ rainbow_web/src/Pages/CommunityPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../Components/Header';
+
+function CommunityPage() {
+  return (
+    <div>
+      <Header />
+      <h1>Community Page</h1>
+      <p>커뮤니티 페이지입니다.</p>
+    </div>
+  );
+}
+
+export default CommunityPage;
