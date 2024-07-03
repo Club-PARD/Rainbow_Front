50ee74558c1aaedebd416f0cd0168ee503ea8051
codms0201 <chanyeol0615@naver.com>
2024-07-03 19:25:36 +0900

    Feat: 메인 헤더 컴포넌트 생성

---
+++ rainbow_web/src/Pages/Book.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Book() {
+  return (
+    <div>
+      <h1>Book Page</h1>
+      <p>자서전 페이지</p>
+    </div>
+  );
+}
+
+export default Book;
---
+++ rainbow_web/src/Pages/List.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function List() {
+  return (
+    <div>
+      <h1>List Page</h1>
+      <p>글 목록 페이지입니다.</p>
+    </div>
+  );
+}
+
+export default List;
