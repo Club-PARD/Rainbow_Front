0c5873506141add8671d5ab1a835614ac20e25ba
Yoomin Hwang <yoominh3@gmail.com>
2024-07-04 15:59:40 +0900

    Feat: Atom.js 추가 및 App.js router 수정

--- rainbow_web/src/App.js
+++ rainbow_web/src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
 import LoginPage from './Pages/LoginPage';
+import {Route, Routes} from "react-router-dom";
+import Main from './Pages/Main';
+import Book from './Pages/Book';
+import List from './Pages/List';
 
 function App() {
   return (
     <div className="App">
-      <LoginPage />
+      <Routes>
+        <Route path="/" element={<LoginPage />}/>
+        <Route path="/main" element={<MainPage />}/>
+        <Route path="/book" element={<Book />}/> 
+        <Route path="/list" element={<List />}/>
+      </Routes>
     </div>
   );
 }
