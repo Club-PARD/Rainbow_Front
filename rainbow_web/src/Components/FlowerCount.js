b88ebc1f8f7899e29aa70fb8711ebb9fbc6d906e
Yoomin Hwang <yoominh3@gmail.com>
2024-07-10 12:31:26 +0900

    Fix: Mainpage ui 구현

---
+++ rainbow_web/src/Components/FlowerCount.js
@@ -0,0 +1,39 @@
+import styled from 'styled-components';
+import {useRecoilState} from 'recoil';
+import { PostCount } from '../Atom';
+import flowerImage from "../Assets/Img/flower.svg";
+
+function FlowerCount () {
+    const [postCount, setPostCount] = useRecoilState(PostCount);
+    
+    return (
+    <FlowerCountWrapper>
+        <img src={flowerImage} alt="flower" />
+        <FlowerText>지금까지 심은 꽃 개수 : {postCount}/40</FlowerText>
+    </FlowerCountWrapper>
+    );
+};
+
+const FlowerCountWrapper = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    
+    width: 362px;
+    margin: 5em;
+    padding: 8px 16px;
+
+    border-radius: 8px;
+    border: 1px solid #C6C6C6;
+    background: var(--grayscale-White, #FEFEFE);
+    box-shadow: 0px 4px 15px 0px rgba(0, 0, 0, 0.15);
+`
+
+const FlowerText = styled.div`
+    color: #9B9B9B;
+    text-align: center;
+    font-size: 16px;
+    font-weight: 500;
+`
+
+export default FlowerCount;
--- rainbow_web/src/Pages/LoginPage.js
+++ rainbow_web/src/Pages/LoginPage.js
@@ -95,7 +95,7 @@ const Container = styled.div`
   align-items: center;
   justify-content: center;
   width: 100%;
-  height: auto;
+  height: 100vh;
 `;
 
 const Wrapper = styled.div`
